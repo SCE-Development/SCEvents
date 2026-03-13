327ea6419f3a8c855efbf9a62fac1d42a4e1024f
maernest04 <maernest04@gmail.com>
2026-03-13 00:17:48 -0700

    Added mongoDB to the project and .gitignore. Everyone should have their own individual data folder in the project folder for local testing

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM golang:1.25-alpine
 WORKDIR /app
 
 COPY go.mod go.sum ./
+COPY pkg/ pkg/
 RUN go mod download
 
 COPY cmd/ cmd/
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	// "github.com/SCE-Development/SCEvents/pkg/types"
 )
 
+var mongoClient *mongo.Client
+var mongoDB *mongo.Database
+
 func main() {
+	uri := os.Getenv("MONGO_URI")
+	if uri == "" {
+		uri = "mongodb://localhost:27017"
+	}
+
+	ctx := context.Background()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		_ = client.Disconnect(ctx)
+	}()
+
+	mongoClient = client
+	mongoDB = client.Database("scevents")
+
 	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
-		// json response
 		c.JSON(http.StatusOK, gin.H{
 			"message": "response",
 		})
